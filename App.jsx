8fadb05a28f75ad60b2cdcde4ae8aef2cc98310f
sreenithiya958-bit <sreenithiya958@gmail.com>
2026-05-10 19:14:39 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -3,50 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { useReducer } from "react";
+const initialState = {
+  count: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+
+    case "DECREMENT":
+      return { count: state.count - 1 };
+
+    case "RESET":
+      return { count: 0 };
+
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [text, setText] = useState("");
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <div
-      style={{
-        backgroundColor: darkMode ? "#222" : "#fff",
-        color: darkMode ? "#fff" : "#000",
-        minHeight: "100vh",
-        padding: "20px",
-      }}
-    >
-      <h3>useState Example</h3>
-      <input
-        type="text"
-        placeholder="Type something..."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        style={{
-          padding: "10px",
-          width: "250px",
-          marginBottom: "20px",
-        }}
-      />
-      <h3>Live Preview:</h3>
-      <p>{text}</p>
+    <div style={{ padding: "20px" }}>
+      <h1>Counter Using useReducer</h1>
 
-      <hr />
+      <h2>Count: {state.count}</h2>
       <button
-        onClick={toggleTheme}
-        style={{
-          padding: "10px 15px",
-          cursor: "pointer",
-        }}
+        onClick={() => dispatch({ type: "INCREMENT" })}
       >
-        Switch to {darkMode ? "Light" : "Dark"} Mode
+        Increment
+      </button>
+      <button
+        onClick={() => dispatch({ type: "DECREMENT" })}
+        style={{ marginLeft: "10px" }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => dispatch({ type: "RESET" })}
+        style={{ marginLeft: "10px" }}
+      >
+        Reset
       </button>
     </div>
   );
 }
 
 export default App;
+
